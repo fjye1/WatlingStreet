09f0ac6f4a8e09e314f4ce9f41b0b34c1fcee9b9
Frederick Elledge <frederickjye@gmail.com>
2026-07-23 18:12:46 +0100

    update compose.yml

--- config.py
+++ config.py
@@ -4,9 +4,18 @@ from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))  # loads .env into os.environ
 
+# Pull credentials from environment
+pg_user = os.environ.get("POSTGRES_USER", "postgres")
+pg_pass = os.environ.get("POSTGRES_PASSWORD", "postgrespassword")
+pg_db = os.environ.get("POSTGRES_DB", "myapp_db")
 
+# Default host to 'localhost' (for running run.py locally outside Docker)
+pg_host = os.environ.get("POSTGRES_HOST", "localhost")
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL",
+        f"postgresql://{pg_user}:{pg_pass}@{pg_host}:5432/{pg_db}"
+    )
     SECRET_KEY = os.environ.get("SECRET_KEY") # or "dev-secret-key"   fallback for dev
     CSFR_KEY = os.environ.get("CSFR")
 
